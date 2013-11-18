7be428a514fed81a91c070190d3dd22651e85fbf
Greg Eng <greg.eng@gmail.com>
2013-11-18 16:03:32 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrackYourTrailer::Application.config.secret_key_base = 'e7005533982cdb9927f9b8e5ecba68f0802cb91dd5c9e1bbe70a19b2811250cfddac8ffaf1883d8e5e03cf5b8960f326ea2428d6183a198f357934396a29188f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrackYourTrailer::Application.config.session_store :cookie_store, key: '_track-your-trailer_session'
