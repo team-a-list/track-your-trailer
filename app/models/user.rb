a694b6c2c0d7133edcfcdb326cf77eeefc7a0220
David Bella <dbella1@gmail.com>
2013-11-19 14:30:57 -0500

    user model helper for emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,19 @@ class User < ActiveRecord::Base
   has_many :user_movies
   has_many :movies, :through => :user_movies
 
-  def self.users_for(movie_list)
-    movie_list.collect(&:users).uniq(&:name)
+  validates :email, :uniqueness => true
+
+  def self.users_for(notify_day = 0)
+    movie_list = Movie.movies_released(notify_day)
+    # movie_list.collect(&:users).uniq(&:name)
+    user_list = []
+
+    movie_list.each do |movie|
+      movie.users.each do |user|
+        user_list << user
+      end
+    end
+
+    user_list
   end
 end
