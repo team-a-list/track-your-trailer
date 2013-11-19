b069047c6d576502677fe394c12c6d5482741b87
Greg Eng <greg.eng@gmail.com>
2013-11-19 18:56:56 -0500

    rotten tomatoe api add movies

--- app/models/rotten_tomatoes_api.rb
+++ app/models/rotten_tomatoes_api.rb
@@ -17,19 +17,16 @@ class RottenTomatoesApi
     HTTParty.get("http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/upcoming.json?apikey=#{API_KEY}&page_limit=5", :format => :json)
   end
 
+  def self.seed_upcoming_movies
+      self.upcoming_movies["movies"].each do |movie|
+        Movie.create_from_rotten(movie)
+    end
+  end
 
-
-# # movie search , we can limit to 1-5 maybe
-# HTTParty.get('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=[API KEY GOES HERE]&q=toy%20story%203&page_limit=5', :format => :json)
-
-# # array of movies by search terms, limited to 5
-# movie_search["movies"]
-
-# # the first movie .. this returns a similar object ... or you could just use the id to grab the specific movie info json page above
-# movie_search["movies"].first["id"]
-
-# HTTParty.get('http://api.rottentomatoes.com/api/public/v1.0/movies/770672122.json?apikey=[API KEY GOES HERE]', :format => :json)
-
-# HTTParty.get('http://api.rottentomatoes.com/api/public/v1.0/movies/10156.json?apikey=[API KEY GOES HERE]', :format => :json)
+  def self.seed_upcoming_dvds
+      self.upcoming_dvds["movies"].each do |movie|
+        Movie.create_from_rotten(movie)
+    end
+  end
 
 end
