2155ca285930243178fa5695cc5aa80a5f4acfc5
David Bella <dbella1@gmail.com>
2013-11-19 14:30:57 -0500

    user model helper for emails

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,12 @@ class Movie < ActiveRecord::Base
   has_many :user_movies
   has_many :users, :through => :user_movies
 
+  def self.movies_released(days_until = 0)
+    theater = Movie.theater_movies_released(days_until)
+    dvd = Movie.dvd_movies_released(days_until)
+    (theater << dvd).flatten
+  end
+
   def self.theater_movies_released(days_until = 0)
     Movie.where(:release_date_theater => Time.zone.now.days_since(days_until).beginning_of_day)
   end
