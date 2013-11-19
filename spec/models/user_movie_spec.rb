a071c589e21bb7e6514cd59308e119ed8ed3a9fd
emilyxxie <emilyxxie@gmail.com>
2013-11-18 19:55:58 -0500

    On a rollllll. Added some more tests

--- spec/models/user_movie_spec.rb
+++ spec/models/user_movie_spec.rb
@@ -1,10 +1,25 @@
 require 'spec_helper'
 
 describe UserMovie do
+
   context "Emily likes Chicago" do
-    it "should have the username" do 
-      user_movie = FactoryGirl.create(:user_movie, :user => FactoryGirl.create(:user), :movie => FactoryGirl.create(:movie))
+    let(:user_movie) {FactoryGirl.create(:user_movie, :user => FactoryGirl.create(:user), :movie => FactoryGirl.create(:movie))}
+
+    it "should be able to access the username" do 
       user_movie.user.name.should eq("Emily")
     end
+
+    it "should be able to access the password" do
+      user_movie.user.password.should eq("test")
+    end
+
+    it "should be able to access the email" do
+      user_movie.user.email.should eq("emily@xie.com")
+    end
+
+    it "should be able to access the movie name" do
+      user_movie.movie.name.should eq("Chicago")
+    end
+
   end
 end
