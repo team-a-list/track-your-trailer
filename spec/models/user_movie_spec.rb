dfc06c798a7721319c7ff686ee8f0f9af00ab4ab
emilyxxie <emilyxxie@gmail.com>
2013-11-18 19:47:31 -0500

    ZOMG I fixed the rspec issue. We were missing the 'it'

--- spec/models/user_movie_spec.rb
+++ spec/models/user_movie_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe UserMovie do
   context "Emily likes Chicago" do
-    user_movie = FactoryGirl.create(:user_movie, :user => FactoryGirl.create(:user), :movie => FactoryGirl.create(:movie))
-    user_movie.user.name.should eq("Emily")
+    it "should have the username" do 
+      user_movie = FactoryGirl.create(:user_movie, :user => FactoryGirl.create(:user), :movie => FactoryGirl.create(:movie))
+      user_movie.user.name.should eq("Emily")
+    end
   end
 end
