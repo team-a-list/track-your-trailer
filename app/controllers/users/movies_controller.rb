5c16b2cd6b9fc35ce22649cb072f78629abf272b
David Bella <dbella1@gmail.com>
2013-11-19 13:16:15 -0500

    Adding 'validation' to the user movie tracking

--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -1,7 +1,7 @@
 class Users::MoviesController < ApplicationController
 
   def create
-    UserMovie.create(:user_id => params[:user_id], :movie_id => params[:movie_id])
+    UserMovie.find_or_create_by(:user_id => params[:user_id], :movie_id => params[:movie_id])
     redirect_to user_path(params[:user_id])
   end
 
