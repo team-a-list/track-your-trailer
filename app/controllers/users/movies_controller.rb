49c13fe6b304dacf987eed778940b225b17166fe
Greg Eng <greg.eng@gmail.com>
2013-11-19 12:36:17 -0500

    add destroy action to user movies page

--- app/controllers/users/movies_controller.rb
+++ app/controllers/users/movies_controller.rb
@@ -5,4 +5,11 @@ class Users::MoviesController < ApplicationController
     redirect_to user_path(params[:user_id])
   end
 
+  def destroy
+    @user_movie = UserMovie.where(:user_id => params[:user_id], :movie_id => params[:id])
+    UserMovie.destroy(@user_movie)
+
+    redirect_to :back
+  end
+
 end
