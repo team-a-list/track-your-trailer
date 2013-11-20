28dea7020e1f0e6e39e292be92213941ada1c901
Greg Eng <greg.eng@gmail.com>
2013-11-20 12:00:55 -0500

    twilio welcome message, normalize phone numbers with phony_rails gem

--- app/models/twilio_api.rb
+++ app/models/twilio_api.rb
@@ -1,14 +1,19 @@
 class TwilioApi
   ACCOUNT_SID = open('lib/assets/.twilio_sid').read()
   AUTH_TOKEN = open('lib/assets/.twilio_auth_token').read()
+  NUMBER = "+15128722533"
+  @CLIENT = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
 
-  def self.test
-    @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
+
+  def welcome_text(user)
     message = @client.account.sms.messages.create(
-      :body => "Remember that movie you wanted to see? It's out today!",
-      :to => "",     # Replace with your phone number
-      :from => "+15128722533")   # Replace with your Twilio number
+      :body => "Welcome to TrackYourTrailer. Enjoy!",
+      :to => user.phone_number,
+      :from => NUMBER )
     puts message.sid
+  end
+
+  def notify(user, notify_day)
   end
 
 end
