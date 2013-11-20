4f816b98a5343ade00292f9a652231bf8e7f9ba7
emilyxxie <emilyxxie@gmail.com>
2013-11-20 13:24:41 -0500

    mailer and text works on welcome. Awww yeah.

--- app/models/twilio_api.rb
+++ app/models/twilio_api.rb
@@ -1,12 +1,12 @@
 class TwilioApi
-  ACCOUNT_SID = open('lib/assets/.twilio_sid').read()
-  AUTH_TOKEN = open('lib/assets/.twilio_auth_token').read()
-  NUMBER = "+15128722533"
+  ACCOUNT_SID = open('lib/assets/.twilio_sid').read
+  AUTH_TOKEN = open('lib/assets/.twilio_auth_token').read
+  NUMBER = "+13476948027"
   @CLIENT = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
 
 
-  def welcome_text(user)
-    message = @client.account.sms.messages.create(
+  def self.welcome_text(user)
+    message = @CLIENT.account.sms.messages.create(
       :body => "Welcome to TrackYourTrailer. Enjoy!",
       :to => user.phone_number,
       :from => NUMBER )
