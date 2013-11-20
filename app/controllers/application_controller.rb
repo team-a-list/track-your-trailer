ed6c58cb8940482599bd3135999ec6fd8d4f899d
emilyxxie <emilyxxie@gmail.com>
2013-11-20 09:51:54 -0500

    minor view changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,10 @@ class ApplicationController < ActionController::Base
   end
 
   def user_logged_in?
+    !!current_user
+  end
+
+  def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
