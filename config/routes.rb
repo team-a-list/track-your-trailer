65494b136f80d9acb8e487b27eb8352303ffa694
Greg Eng <greg.eng@gmail.com>
2013-11-19 19:00:41 -0500

    make movies index the root page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ TrackYourTrailer::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'movies#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
