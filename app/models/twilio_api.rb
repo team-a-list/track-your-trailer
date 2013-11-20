6fd0c1e6d6c497f7c4b64835937363537da77a93
Greg Eng <greg.eng@gmail.com>
2013-11-20 11:07:21 -0500

    make text messages work via twilio api

--- app/models/twilio_api.rb
+++ app/models/twilio_api.rb
@@ -1,2 +1,14 @@
 class TwilioApi
+  ACCOUNT_SID = open('lib/assets/.twilio_sid').read()
+  AUTH_TOKEN = open('lib/assets/.twilio_auth_token').read()
+
+  def self.test
+    @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
+    message = @client.account.sms.messages.create(
+      :body => "Remember that movie you wanted to see? It's out today!",
+      :to => "",     # Replace with your phone number
+      :from => "+15128722533")   # Replace with your Twilio number
+    puts message.sid
+  end
+
 end
