26e65f99393523377de1fc5b84a74ef9611fa626
emilyxxie <emilyxxie@gmail.com>
2013-11-20 14:43:46 -0500

    user show page logic, refactored logged in message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :user_movies
   has_many :movies, :through => :user_movies
+  
   has_secure_password
 
   phony_normalize :phone_number, :default_country_code => "US"
