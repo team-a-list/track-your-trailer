7c3e2406087954c260e885eaf89251c1fab18907
David Bella <dbella1@gmail.com>
2013-11-20 10:16:03 -0500

    Added simple authorization and user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
 
+  skip_before_action :login_required, :only => [:new, :create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      login(user.id)
+      redirect_to root_path
+    else
+      redirect_to :back
+    end
+  end
+
   def index
     @users = User.all
   end
@@ -8,4 +24,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
