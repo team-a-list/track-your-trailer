8e3073e3e0c38481a4d4c04fbee618b2f46bb8d5
David Bella <dbella1@gmail.com>
2013-11-21 12:40:09 -0500

    Changing to 5 minutes for testing on Heroku

--- lib/clock.rb
+++ lib/clock.rb
@@ -11,4 +11,4 @@ load "#{Rails.root}/lib/tasks/notifications.rake"
 # Rake.application = rake
 # Rake::Task.define_task = (:environment)
 # args = Rake::TaskArguments.new({:days => 1, [:days] => 1})
-every(5.seconds, 'Running email notifier', :at => 'Thursday 17:39', :tz => 'UTC') { Rake::Task["notify:email"].execute(1) }
+every(5.minutes, 'Running email notifier', :at => 'Thursday 17:45', :tz => 'UTC') { Rake::Task["notify:email"].execute(1) }
