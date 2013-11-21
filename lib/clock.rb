1cd961e48172462ecf5c2130ec064d9675dcf3fd
David Bella <dbella1@gmail.com>
2013-11-21 12:57:34 -0500

    Updating to use weekly on thursday email notifier for Friday movies

--- lib/clock.rb
+++ lib/clock.rb
@@ -11,4 +11,4 @@ load "#{Rails.root}/lib/tasks/notifications.rake"
 # Rake.application = rake
 # Rake::Task.define_task = (:environment)
 # args = Rake::TaskArguments.new({:days => 1, [:days] => 1})
-every(5.minutes, 'Running email notifier', :at => 'Thursday 17:45', :tz => 'UTC') { Rake::Task["notify:email"].execute(1) }
+every(1.week, 'Running email notifier', :at => 'Thursday 18:00', :tz => 'UTC') { Rake::Task["notify:email"].execute(1) }
