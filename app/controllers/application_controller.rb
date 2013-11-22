fdc7b2b9628440b9b31a4248c86cb05cf365824d
emilyxxie <emilyxxie@gmail.com>
2013-11-22 16:38:23 -0500

    devise update user changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :phone_number, :email, :current_password, :password) }
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :phone_number, :email, :password) }
   end
   
