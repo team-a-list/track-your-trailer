dcfa9f4cb9c8538a533331017b66c198d62f5537
emilyxxie <emilyxxie@gmail.com>
2013-11-22 15:13:27 -0500

    more devise amazingness

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       TwilioApi.welcome_text(user)
-      UserMailer.welcome_email(user).deliver!
       login(user.id)
       redirect_to root_path
     else
