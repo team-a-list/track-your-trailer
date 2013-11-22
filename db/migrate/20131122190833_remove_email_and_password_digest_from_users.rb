e9851ba49c3c2b43ffebb37118483fbf030c49a4
emilyxxie <emilyxxie@gmail.com>
2013-11-22 16:06:41 -0500

    database changes

--- db/migrate/20131122190833_remove_email_and_password_digest_from_users.rb
+++ db/migrate/20131122190833_remove_email_and_password_digest_from_users.rb
@@ -1,5 +1,6 @@
 class RemoveEmailAndPasswordDigestFromUsers < ActiveRecord::Migration
   def change
     remove_column :users, :password_digest, :string
+    remove_column :users, :email, :string
   end
 end
