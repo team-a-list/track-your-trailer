f6c688a83bd6f11edd8bdc4de87578a7eb028812
Greg Eng <greg.eng@gmail.com>
2013-11-22 11:18:41 -0500

    carousel replaces home page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,8 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.order(release_date_theater: :desc).limit(10)
+    @first_movie = @movies.first
+    @other_movies = @movies[1..-1]
   end
 
   def all_movies
