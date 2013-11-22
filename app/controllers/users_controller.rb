8db908bc51b23927a362f65c935ff2fa2793b13e
emilyxxie <emilyxxie@gmail.com>
2013-11-22 16:40:22 -0500

    cleaned up code in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,5 @@
 class UsersController < ApplicationController
 
-  # skip_before_action :login_required, :only => [:new, :create]
-
-  # def new
-  #   @user = User.new
-  # end
-
-  # def create
-  #   user = User.new(user_params)
-  #   if user.save
-  #     TwilioApi.welcome_text(user)
-  #     login(user.id)
-  #     redirect_to root_path
-  #   else
-  #     redirect_to :back
-  #   end
-  # end
-
   def index
     # if admin
     @users = User.all
@@ -30,8 +13,4 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :phone_number)
-  end
 end
