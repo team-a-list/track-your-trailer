6468af1bedf6247dff07103e9d3aa1e629656409
Greg Eng <greg.eng@gmail.com>
2013-11-26 14:28:29 -0500

    redirect to root after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
-  before_filter :configure_permitted_parameters, if: :devise_controller? 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
@@ -13,5 +13,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :phone_number, :email, :current_password, :password) }
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :phone_number, :email, :password) }
   end
-  
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
 end
