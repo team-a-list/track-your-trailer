c526a794af9ca8948effdd33f19986fe9d69fa94
Greg Eng <greg.eng@gmail.com>
2014-07-26 15:37:53 -0700

    add additonal exclusion urls to the default posters for movies index and all upcoming movies pages

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,13 +1,15 @@
 class MoviesController < ApplicationController
-
   skip_before_filter :authenticate_user!, :only => [:index, :all_movies, :show]
+  DEFAULT_POSTER_IMAGES = ["http://images.rottentomatoescdn.com/images/redesign/poster_default.gif",
+                           "http://d1rjibvava1hwe.cloudfront.net/static/images/redesign/poster_default.gif",
+                           "http://d1rjibvava1hwe.cloudfront.net/static/images/redesign/poster_default_thumb.gif"]
 
   def index
-    @movies = Movie.where.not(poster_image: "http://images.rottentomatoescdn.com/images/redesign/poster_default.gif").where("release_date_theater is not null").order(release_date_theater: :desc).limit(9).reverse
+    @movies = Movie.where.not(poster_image: DEFAULT_POSTER_IMAGES ).where("release_date_theater is not null").order(release_date_theater: :desc).limit(9).reverse
   end
 
   def all_movies
-    @movies = Movie.all.where.not(poster_image: "http://images.rottentomatoescdn.com/images/redesign/poster_default.gif").where("release_date_theater > ? or release_date_dvd > ?", Time.now, Time.now)
+    @movies = Movie.all.where.not(poster_image: DEFAULT_POSTER_IMAGES).where("release_date_theater > ? or release_date_dvd > ?", Time.now, Time.now)
   end
 
   def show
