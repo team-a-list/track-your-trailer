4c5ad05756d929a1654159392a2de9d5ef9216a9
Greg Eng <greg.eng@gmail.com>
2014-08-01 23:33:07 -0700

    remove welcome email method on user mail no longer being used. write spec for remaining methods in the user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,6 @@
 class UserMailer < ActionMailer::Base
   default from: "trackyourtrailer@gmail.com"
 
-  def welcome_email(user)
-    @user = user
-    mail(to: user.email, subject: 'Welcome to TrackYourTrailer')
-  end
-
   def notify(user, notify_day)
     @user = user
     @notify_day = notify_day
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,5 +1,13 @@
 require "spec_helper"
 
 describe UserMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "e-mail notification types" do
+    it "is able to send a reminder email message on release days" do
+      user = create(:user)
+      mail = UserMailer.notify(user, 0)
+      expect(mail.from.first).to eq("trackyourtrailer@gmail.com")
+      expect(mail.to.first).to eq("greg.eng@gmail.com")
+      expect(mail.subject).to eq("TrackYourTrailer - It's finally out!")
+    end
+  end
 end
