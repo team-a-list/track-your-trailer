cfc33b053e95f08b469ef71f5028439373297ce1
Greg Eng <greg.eng@gmail.com>
2014-08-02 10:07:56 -0700

    add comment pseudo code for testing user movies controller

--- spec/controllers/users/movies_controller_spec.rb
+++ spec/controllers/users/movies_controller_spec.rb
@@ -2,4 +2,14 @@ require 'spec_helper'
 
 describe Users::MoviesController do
 
+  describe 'POST#CREATE' do
+    # context 'with valid attributes' do
+    #   it 'creates a new user_movie' do
+    #     expect {
+    #       post :create, user_movie
+    #     }.to change(UserMovie,:count).by(1)
+    #   end
+    # end
+  end
+
 end
