bf834356a77847056062d7d1a0efb2c5035af111
Greg Eng <greg.eng@gmail.com>
2014-08-02 11:16:57 -0700

    write spec for upcoming dvds

--- spec/models/rotten_tomatoes_api_spec.rb
+++ spec/models/rotten_tomatoes_api_spec.rb
@@ -2,25 +2,55 @@ require 'spec_helper'
 
 describe RottenTomatoesApi do
   describe 'general content query methods' do
-    context "searching" do
-      it 'should allow searches with valid search strings' do
-        search_string = "Toy Story"
-        search_results = RottenTomatoesApi.search(search_string)
+    it 'should allow searches with valid search strings' do
+      search_string = "Toy Story"
+      search_results = RottenTomatoesApi.search(search_string)
 
-        expect(search_results.class).to eq(HTTParty::Response)
-        expect(search_results).to have_key("movies")
-        expect(search_results["movies"].length).to be <= (6)
-      end
-
-      it 'should not error if no movies are found via search' do
-        search_string = "1NVAL1D M0VI3"
-        search_results = RottenTomatoesApi.search(search_string)
-
-        expect(search_results.class).to eq(HTTParty::Response)
-        expect(search_results).to have_key("movies")
-        expect(search_results["movies"].length).to eq(0)
-      end
+      expect(search_results.class).to eq(HTTParty::Response)
+      expect(search_results).to have_key("movies")
+      expect(search_results["movies"].length).to be <= (6)
     end
+
+    it 'should not error if no movies are found via search' do
+      search_string = "1NVAL1D M0VI3"
+      search_results = RottenTomatoesApi.search(search_string)
+
+      expect(search_results.class).to eq(HTTParty::Response)
+      expect(search_results).to have_key("movies")
+      expect(search_results["movies"].length).to eq(0)
+    end
+
+    it 'should query rotten tomatoes for upcoming theater releases' do
+      upcoming_theater_releases = RottenTomatoesApi.upcoming_movies
+
+      expect(upcoming_theater_releases.class).to eq(HTTParty::Response)
+      expect(upcoming_theater_releases).to have_key("movies")
+
+      upcoming_movie = upcoming_theater_releases["movies"][0]
+      expect(upcoming_movie.class).to eq(Hash)
+      expect(upcoming_movie).to have_key("release_dates")
+      expect(upcoming_movie["release_dates"]).to have_key("theater")
+
+      theater_release_date = upcoming_movie["release_dates"]["theater"]
+      expect(theater_release_date.to_time).to be > Time.now
+    end
+
+    it 'should query rotten tomatoes for upcoming dvd releases' do
+      upcoming_dvd_releases = RottenTomatoesApi.upcoming_dvds
+
+      expect(upcoming_dvd_releases.class).to eq(HTTParty::Response)
+      expect(upcoming_dvd_releases).to have_key("movies")
+
+      upcoming_movie = upcoming_dvd_releases["movies"][0]
+      expect(upcoming_movie.class).to eq(Hash)
+      expect(upcoming_movie).to have_key("release_dates")
+      expect(upcoming_movie["release_dates"]).to have_key("dvd")
+
+      dvd_release_date = upcoming_movie["release_dates"]["dvd"]
+      expect(dvd_release_date.to_time).to be > Time.now
+    end
+
+
 
   end
 
