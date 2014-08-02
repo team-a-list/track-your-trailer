a12ba3963dbc289ac45266e165b813713d70b78e
Greg Eng <greg.eng@gmail.com>
2014-08-02 10:57:30 -0700

    write one search spec for the rotten tomatoes api

--- spec/models/rotten_tomatoes_api_spec.rb
+++ spec/models/rotten_tomatoes_api_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe RottenTomatoesApi do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'general content query methods' do
+    context "searching" do
+      it 'should allow searches with valid search strings' do
+        search_string = "Toy Story"
+        search_results = RottenTomatoesApi.search(search_string)
+
+        expect(search_results.class).to eq(HTTParty::Response)
+        expect(search_results.length).to be <= (6)
+        expect(search_results).to have_key("movies")
+      end
+    end
+
+  end
+
+
+  describe 'content seeding methods' do
+  end
+
+  describe 'content updating methods' do
+  end
+
 end
