04cc5380a2e4798ebb07624c5265e7b465fff00b
Greg Eng <greg.eng@gmail.com>
2014-08-03 20:51:56 -0700

    reorganize movie class

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,20 +5,6 @@ class Movie < ActiveRecord::Base
   validates :name, :rotten_tomatoes_uri, :presence => true
   validates :rotten_tomatoes_uri, :uniqueness => true
 
-  def self.movies_released(days_until = 0)
-    theater = Movie.theater_movies_released(days_until)
-    dvd = Movie.dvd_movies_released(days_until)
-    theater.concat(dvd)
-  end
-
-  def self.theater_movies_released(days_until = 0)
-    Movie.where(:release_date_theater => Time.zone.now.days_since(days_until).beginning_of_day)
-  end
-
-  def self.dvd_movies_released(days_until = 0)
-    Movie.where(:release_date_dvd => Time.zone.now.days_since(days_until).beginning_of_day)
-  end
-
   def self.create_from_rotten(movie_hash)
     original_movie_poster = defend_against_thumbnail_images(movie_hash["posters"]["original"])
 
@@ -32,10 +18,6 @@ class Movie < ActiveRecord::Base
         :synopsis => movie_hash["synopsis"],
         :rotten_tomatoes_link => movie_hash["links"]["alternate"]
       )
-  end
-
-  def self.find_movies_without_release_dates
-    self.where("release_date_dvd is null OR release_date_theater is null")
   end
 
   def update_from_rotten(movie_hash)
@@ -54,6 +36,24 @@ class Movie < ActiveRecord::Base
   end
 
   private
+
+  def self.movies_released(days_until = 0)
+    theater = Movie.theater_movies_released(days_until)
+    dvd = Movie.dvd_movies_released(days_until)
+    theater.concat(dvd)
+  end
+
+  def self.theater_movies_released(days_until = 0)
+    Movie.where(:release_date_theater => Time.zone.now.days_since(days_until).beginning_of_day)
+  end
+
+  def self.dvd_movies_released(days_until = 0)
+    Movie.where(:release_date_dvd => Time.zone.now.days_since(days_until).beginning_of_day)
+  end
+
+  def self.find_movies_without_release_dates
+    self.where("release_date_dvd is null OR release_date_theater is null")
+  end
 
   def self.defend_against_thumbnail_images(poster_url)
     if poster_url.include?('tmb')
