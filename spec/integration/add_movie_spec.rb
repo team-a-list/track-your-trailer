4d536213f2875c7c2daa75c17f5473983575ad70
Greg Eng <greg.eng@gmail.com>
2014-08-03 20:34:40 -0700

    refactor add movie integration spec

--- spec/integration/add_movie_spec.rb
+++ spec/integration/add_movie_spec.rb
@@ -1,40 +1,25 @@
 require 'spec_helper'
 
-### TODO Refactor repeated code below
-
 feature 'Add a movie to my movies' do
+  before(:each) do
+    @movie = create(:movie)
+  end
+
   scenario 'successfully adds a movie' do
-    create(:confirmed_user)
-    movie = create(:movie)
-
-    visit root_path
-    click_link('top-nav-login')
-    fill_in('Email', :with => 'greg.eng@gmail.com')
-    fill_in('Password', :with => 'password')
-    click_button('Sign in')
-    expect(page).to have_css '.notice', text: 'Signed in successfully.'
-
-    visit movie_path(movie)
-    click_link('Add This Movie')
+    sign_in
+    add_a_movie(@movie)
     expect(page).to have_css 'h2', text: 'My Movies:'
   end
 
   scenario 'successfully removes a movie' do
-    create(:confirmed_user)
-    movie = create(:movie)
-
-    visit root_path
-    click_link('top-nav-login')
-    fill_in('Email', :with => 'greg.eng@gmail.com')
-    fill_in('Password', :with => 'password')
-    click_button('Sign in')
-    expect(page).to have_css '.notice', text: 'Signed in successfully.'
-
-    visit movie_path(movie)
-    click_link('Add This Movie')
-    expect(page).to have_css 'h2', text: 'My Movies:'
-
+    sign_in
+    add_a_movie(@movie)
     click_link('Remove From My List')
     expect(page).to_not have_css '.profile-movie'
+  end
+
+  def add_a_movie(movie)
+    visit movie_path(movie)
+    click_link('Add This Movie')
   end
 end
